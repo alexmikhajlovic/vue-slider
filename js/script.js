cf27693d10215b264862712585ce7206091178e1
Alex Greco <alexmikhajlovic@gmail.com>
2021-01-26 16:42:12 +0100

    Upgrade

--- js/script.js
+++ js/script.js
@@ -8,16 +8,23 @@ var app = new Vue({
             'img/img2.jpg',
             'img/img3.jpg',
             'img/img4.jpg',
-            'img/img5.gif',
-        ]
+        ],
+        interval: '',
     },
+    created(){
+        this.interval = setInterval(this.nextImg,3000);
+    }, 
     methods: {
         nextImg(){
             (this.counter == this.images.length -1) ? this.counter = 0 :this.counter++;
         },
         prevImg(){
             (this.counter < 1) ? this.counter = (this.images.length -1) :this.counter--;
-        }
+        },
+        changeImg(i){
+            this.counter = i;
+            clearInterval(this.interval);
+        },
     },
     
 });
